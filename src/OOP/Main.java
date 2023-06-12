cec90dd307ecceb6b5fd3924c88bc99595b585e9
Alberts Lagzdins <albertslagzdins1@gmail.com>
2023-06-13 00:07:17 +0300

    Initail commit 2

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -1,10 +1,9 @@
 package OOP;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
+		new Ramis();
 	}
-
 }
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Overlord
- *
- */
-module Tests {
-}
