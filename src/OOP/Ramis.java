bec08b2c003f835cd133a7d0876f7cf9f3ee86fa
Alberts Lagzdins <albertslagzdins1@gmail.com>
2023-06-13 01:12:11 +0300

    Izveidots sakuma ekrāns

--- src/OOP/Ramis.java
+++ src/OOP/Ramis.java
@@ -1,15 +1,73 @@
 package OOP;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Ramis extends JFrame implements KeyListener {
+	static boolean wNospiests;
+	static boolean sNospiests;
+	static boolean enterNospiests;
+	
  Ramis(){
-	 
+	    this.addKeyListener(this);   
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.getContentPane().setBackground(Color.black);
+		this.setLayout(null);
+		this.setVisible(true);
+		sakums(this);
  }
-
+public static void sakums(JFrame ramis) {
+	int option;
+	boolean turpinat = false;
+	new Font("Liels", Font.PLAIN, 40);
+	new Font("Videjs", Font.PLAIN, 25);
+	
+	JLabel Tituls= new JLabel();
+	Tituls.setText("Tests-OOP");
+	Tituls.setBounds(10,0,200,200);
+	Tituls.setForeground(Color.WHITE);
+	Tituls.setFont(new Font("Liels", Font.PLAIN, 40));
+	
+	JLabel Starts= new JLabel();
+	Starts.setText("Sakt testu");
+	Starts.setBounds(10,100,200,200);
+	Starts.setFont(new Font("Videjs", Font.PLAIN, 25));
+	Starts.setForeground(Color.WHITE);
+	
+	JLabel Opcijas= new JLabel();
+	Opcijas.setText("Opcijas");
+	Opcijas.setBounds(10,150,200,200);
+	Opcijas.setFont(new Font("Videjs", Font.PLAIN, 25));
+	Opcijas.setForeground(Color.WHITE);
+	
+	JLabel Izziet= new JLabel();
+	Izziet.setText("Izziet");
+	Izziet.setBounds(10,200,200,200);
+	Izziet.setFont(new Font("Videjs", Font.PLAIN, 25));
+	Izziet.setForeground(Color.WHITE);
+	
+	JLabel Raksturojums= new JLabel();
+	JLabel Atpakal= new JLabel();
+	ramis.add(Tituls);
+	ramis.add(Starts);
+	ramis.add(Opcijas);
+	ramis.add(Izziet);
+	do {
+		
+	}while(turpinat!=true);
+}
+public static void jautajumi() {
+	
+}
+public static void atbildes() {
+	
+}
 @Override
 public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
@@ -19,12 +77,18 @@ public void keyTyped(KeyEvent e) {
 @Override
 public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
-	
+	switch(e.getKeyCode()) {
+	case 87: wNospiests=true;
+		break;
+	case 83: sNospiests=true;
+		break;
+	case 10: enterNospiests=true;
+		break;
 }
-
+}
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
-	
+	System.out.println("Nospiesta charactera kods"+e.getKeyCode());
 }
 }
