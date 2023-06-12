10866250309f323d9e9c7ec329c7ac1e40e99f2c
Alberts Lagzdins <albertslagzdins1@gmail.com>
2023-06-13 00:09:19 +0300

    Izveidota ramis klase

---
+++ src/OOP/Ramis.java
@@ -0,0 +1,30 @@
+package OOP;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class Ramis extends JFrame implements KeyListener {
+ Ramis(){
+	 
+ }
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
